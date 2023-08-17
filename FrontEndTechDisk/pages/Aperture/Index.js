6e3cf0d442219a58889fea3667aa5451b639cefd
GabrielAlbanez <gabriel.g.abanez@gmail.com>
2023-08-17 12:22:28 -0300

    firs tela more install lib animations

--- FrontEndTechDisk/pages/Aperture/Index.js
+++ FrontEndTechDisk/pages/Aperture/Index.js
@@ -1,13 +1,23 @@
-import { View, Text, StyleSheet,ImageBackground } from 'react-native'
-import imgBack from "../../assets/imgs/backImg.jpg"
+import { View, Text, StyleSheet,ImageBackground,Image,TouchableOpacity } from 'react-native'
+import Logo from "../../assets/imgs/Logo.png"
 import React from 'react'
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Entypo from 'react-native-vector-icons/Entypo';
+import { AntDesign } from '@expo/vector-icons'; 
 
 export default function Aperture() {
   return (
     <View style={styles.container}>
-      <ImageBackground source={imgBack} resizeMode='cover'  style={styles.img}>
-        <Text style={styles.Text}>Aperture</Text>
-      </ImageBackground>
+      <View style={styles.Logo}>
+        <Image source={Logo} style={styles.LogoImg} />
+      </View>
+      <View style={styles.footer}>
+         <Text style={styles.Text}>CLIQUE ABAIXO</Text>
+         <TouchableOpacity style={styles.buttoEntar}>
+         <AntDesign name="right" size={80} color="black" />
+         </TouchableOpacity>
+      </View>
+      
     </View>
   )
 }
@@ -19,25 +29,45 @@ const styles = StyleSheet.create({
     backgroundColor: "white"
     
   },
-  bigBlue: {
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 30,
+
+  Logo : {
+    width : "100%",
+    height : "55%",
+    display : "flex",
+    alignItems : "center",
+    justifyContent : "center",
   },
-  red: {
-    color: 'red',
- },
- img : {
-  flex: 1,
-  paddingTop : 50,
-  alignItems: 'center',
-  opacity : 0.6,
-  backgroundColor : 'black'
- },
+
+  LogoImg : {
+    width : "70%",
+    height : "65%",
+    borderRadius : 200
+  },
+
+  footer : {
+    width : "100%",
+    height : "100%",
+    display : "flex",
+    alignItems : "center",
+  },
+
+  buttoEntar : {
+     backgroundColor : "white",
+     borderRadius : 300,
+     height : "23%",
+     width : "45%",
+     shadowColor: 'black',
+     shadowRadius: 20,
+     borderWidth: 0.5,
+     elevation: 5,
+     alignItems : "center",
+     justifyContent : "center"
+     
+  },
+
  Text : {
-  fontSize: 42,
+  fontSize: 35,
   lineHeight: 84,
-  fontWeight: 'bold',
   textAlign: 'center',
  }
 })
